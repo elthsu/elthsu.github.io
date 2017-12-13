3e1171d8497457a60d10ede46955cdba12deaf9d
Elton Hsu <elthsu@ucla.edu>
2017-12-13 10:51:16 -0800

    Update routes.js

--- app/config/routes.js
+++ app/config/routes.js
@@ -36,7 +36,7 @@ var Projects = require("../components/Projects");
 module.exports = (
 
   // The high level component is the Router component
-  <Router history={hashHistory}>
+  <Router history={broswerHistory}>
     <Route path="/" component={Main}>
 
       {/* User selection shows the appropriate component */}
